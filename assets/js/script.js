906eb0bc86f30f02d335e313095356792954e041
cbriseida1 <cbriseida1@yahoo.com>
2016-11-28 16:55:59 -0800

    Official

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("li a").hover(function(){
+    $(this).css("color", "#2A2C31");
+	}, function(){
+	$(this).css("color", "white");
+	});
+	$("#about-text-header").hover(function(){
+    $(this).css("color", "#9A927B");
+	}, function(){
+	$(this).css("color", "white");
+	});
+	$("#people-text-header").hover(function(){
+    $(this).css("color", "#9A927B");
+	}, function(){
+	$(this).css("color", "white");
+	});
+});
